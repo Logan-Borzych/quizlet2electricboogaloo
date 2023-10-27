59a77a8714ae3018fadaf0accfaa12113854c78e
Logan Borzych <61635669+Logan-Borzych@users.noreply.github.com>
2023-10-27 11:45:41 -0500

    Added more routes and finished term class.

--- start.py
+++ start.py
@@ -9,20 +9,28 @@ db = SQLAlchemy(app)
 
 class Term(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    setID = db.Column(db.Integer)
     term = db.Column(db.String(255), nullable=False)
+    definiition = db.Column(db.String(255), nullable=False)
 
+    def __repr__(self):
+        return '<name %r>' % self.id
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('index.html')
 
-@app.route('/features')
+@app.route('/features', methods=['GET','POST'])
 def features():
     return render_template('featureselect.html')
 
-@app.route('/sets')
+@app.route('/sets', methods=['GET','POST'])
 def sets():
+    if request.method == "POST"
+        term = request.form["question"]
+        definitionA = request.form["answer"]
     return render_template('flashcards.html')
+
 
 if __name__ == '__main__':
     app.run()
