fa776299514a6ad47acc2305c3829681eaa907eb
loganborzych <loganborzych@gmail.com>
2023-11-09 23:03:43 -0600

    almost done fixing the edit and delete buttons

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, logging
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+import logging
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///terms.db'
 
 db = SQLAlchemy(app)
@@ -15,18 +16,14 @@ class Term(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     term = db.Column(db.String(255), nullable=False)
     definition = db.Column(db.String(255), nullable=False)
-    setName = db.Column(db.String(255), nullable=False)
+    set_id = db.Column(db.Integer, db.ForeignKey('set.id'), nullable=False)
 
-    #i still dont know what this means
-    def __repr__(self):
-        return '<name %r>' % self.id
 
-class set_list(db.Model):
-    set_id = db.Column(db.Integer, primary_key=True)
-    set_name = db.Column(db.String, nullable=False)
+class Set(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    terms = db.relationship('Term', backref='set', lazy=True)
 
-    def __repr__(self):
-        return '<name %r>' % self.id
 
 
 #ROUTES TO RENDER WEBPAGE AND DETERMINE ROUTES FOR WEBPAGES
@@ -51,58 +48,67 @@ def createPage():
 def create():
     return render_template('create.html')
 
+# create_set route
 @app.route('/create_set', methods=['GET', 'POST'])
 def create_set():
     if request.method == "POST":
         
         set_name = request.form['name']
 
-        new_set = set_list(set_name=set_name)
+        new_set = Set(name=set_name)
 
         try:
             db.session.add(new_set)
             db.session.commit()
-            print(redirect(url_for('specific_sets', name=set_name)))
+
+            app.logger.debug(f"Redirecting to: {url_for('specific_sets', set_id=new_set.id)}")
+            
+            # Use url_for to generate the URL for  specific_sets
+            return redirect(url_for('specific_sets', set_id=new_set.id))
         except Exception as e:
-            print(f"Error: {e}")
+            app.logger.error(f"Error: {e}")
             return "check console"
     else:
-        render_sets = set_list.query.order_by(set_list.set_id)
-    return render_template('createSet.html')
+        return render_template('create_set.html')
+
+
+
 
 #currently goes to flashcard page ----------- in future i think we should split
 #                                             this into term creation and flashcards
-@app.route('/sets/<string:name>', methods=['GET','POST'])
-def specific_sets(name):
+@app.route('/sets/<int:set_id>', methods=['GET', 'POST'])
+def specific_sets(set_id):
     if request.method == "POST":
-
-        #grabs term and definition
         termForm = request.form['term']
         definitionForm = request.form['definition']
-        setForm = request.form['setName']
-
-        #creates new Term objects and adds to database
-        pair = Term(term=termForm, definition=definitionForm, setName=setForm)
-
-        #pushes Term object to the database
+        
+        # Query the set based on its ID
+        set_data = Set.query.get_or_404(set_id)
+        
+        # Create a new Term object and add it to the database
+        pair = Term(term=termForm, definition=definitionForm, set_id=set_id)
+        
         try:
             db.session.add(pair)
             db.session.commit()
-            return redirect(url_for('specific_sets'))
+            return redirect(url_for('specific_sets', set_id=set_id))
         except Exception as e:
             print(f"Error: {e}")
-            return "so like, we messed up big time"
+            return "Oops! Something went wrong."
 
     else:
-        #loads database terms
-        pairs = Term.query.order_by(Term.id)
-        return render_template('sets.html', pairs=pairs)
+        # Load database terms based on the set ID
+        set_data = Set.query.get_or_404(set_id)
+        terms = Term.query.filter_by(set_id=set_id).all()
+        return render_template('sets.html', set_data=set_data, terms=terms, set_id=set_id)
+
+
 
 #allows the user to update their database entry
-@app.route('/sets/<string:set_name>/edit/<int:id>', methods=['GET', 'POST'])
-def editPair(id, set_name):
+@app.route('/sets/<string:set_id>/edit/<int:term_id>', methods=['GET', 'POST'])
+def edit_pair(term_id, set_id):
     # Requests the database entry for the pair that the user wants to update
-    pairToUpdate = Term.query.get_or_404(id)
+    pairToUpdate = Term.query.get_or_404(term_id)
 
     # When the user edits the pair, commit to the database
     if request.method == "POST":
@@ -111,22 +117,24 @@ def editPair(id, set_name):
         pairToUpdate.setName = request.form['setName']
         try:
             db.session.commit()
-            return redirect('/sets')
-        except:
+            return redirect(url_for('specific_set', set_id=set_id))
+        except Exception as e:
+            print(f"Error: {e}")
             return "Oops"
     else:
         # Activates upon arrival to the page
         return render_template('editPair.html', Term=pairToUpdate)
 
 #deletes a pair
-@app.route('/delete/<int:id>')
-def deletePari(id):
-    deletePair = Term.query.get_or_404(id)
+@app.route('/sets/<int:set_id>/delete/<int:term_id>')
+def delete_pair(term_id, set_id):
+    delete_pair = Term.query.get_or_404(term_id)
     try:
-        db.session.delete(deletePair)
+        db.session.delete(delete_pair)
         db.session.commit()
-        return redirect('/sets')
-    except:
+        return redirect(url_for('specific_set', set_id=set_id))
+    except Exception as e:
+        print(f"Error: {e}")
         return "Oops"
     
 
@@ -150,4 +158,9 @@ def explore():
     return render_template('explore.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
     app.run()
