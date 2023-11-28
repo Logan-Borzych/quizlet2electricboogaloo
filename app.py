5364e6194a0de2e864eb2fd887e4bc21a9386359
Logan Borzych <61635669+Logan-Borzych@users.noreply.github.com>
2023-11-28 09:10:18 -0600

    fuzzy wyzzy searches

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, logging
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from fuzzywuzzy import fuzz, process
 import logging
 app = Flask(__name__)
 
@@ -70,16 +71,34 @@ def create_set():
     else:
         return render_template('create_set.html')
 
+
+#i have no idea what this does dont touch it
 @app.route('/sets', methods=['GET', 'POST'])
 def sets_main():
     if request.method == 'POST':
         query = request.form.get('query', '')
-        sets = Set.query.filter(Set.name.ilike(f'%{query}%')).all()
+        all_sets = Set.query.all()
+
+        # Use fuzzy matching to find sets with names similar to the query
+        sets_with_scores = [(set_obj, process.extractOne(query, [set_obj.name])) for set_obj in all_sets]
+
+        # Filter out sets with a score below a certain threshold (e.g., 70)
+        threshold = 70
+        sets = [set_obj for set_obj, score in sets_with_scores if score[1] >= threshold]
+
+        # Perform an exact match check
+        exact_matches = Set.query.filter_by(name=query).all()
+        sets_with_scores += [(set_obj, (100, 0)) for set_obj in exact_matches]
+
+        # Sort the results by similarity score
+        sets_with_scores = sorted(sets_with_scores, key=lambda x: x[1][1], reverse=True)
+        sets = [set_obj for set_obj, score in sets_with_scores if score[1] >= threshold]
+
         return render_template('sets_main.html', sets=sets, query=query)
+    
     else:
         # Render the initial sets_main page
         return render_template('sets_main.html', sets=None, query=None)
-
 @app.route('/sets/<int:set_id>', methods=['GET', 'POST'])
 def specific_sets(set_id):
     if request.method == "POST":
